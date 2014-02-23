e572bb220dd6e37fc7426a7d904346f4c76a939e
Daniel Kasper <dankasper@gmail.com>
2014-02-23 00:19:44 -0800

    Removed unnecessary documentation, added task that stops nginx if necessary before restarting it

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,61 +3,32 @@ lock '3.1.0'
 
 set :application, 'danielkasper.net'
 set :repo_url, 'git@github.com:dankasper/home.git'
-set :scm, :git
 set :deploy_to, '/srv/home'
-set :default_env, path:   "#{deploy_to}/sbin:#{deploy_to}/go/bin:$PATH",
+set :default_env, path:   "#{deploy_to}/sbin:#{deploy_to}/go/bin",
                   goroot: "#{deploy_to}/go"
-
-# Default branch is :master
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
-
-# Default deploy_to directory is /var/www/my_app
-# set :deploy_to, '/var/www/my_app'
-
-# Default value for :scm is :git
-# set :scm, :git
-
-# Default value for :format is :pretty
-# set :format, :pretty
-
-# Default value for :log_level is :debug
-# set :log_level, :debug
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# set :linked_files, %w{config/database.yml}
-
-# Default value for linked_dirs is []
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
 
 namespace :deploy do
 
+  before :restart, :stop_nginx do
+    on roles(:app) do
+      execute <<-COMMAND
+        cd #{deploy_to}
+	if [ -f logs/nginx.pid ]
+    	  then sbin/nginx -s stop
+	fi
+      COMMAND
+    end
+  end
+
   desc 'Restart application'
   task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
+    on roles(:app) do
       within fetch(:deploy_to) do
-        execute 'sbin/nginx'
+        execute 'nginx'
       end
     end
   end
 
   after :publishing, :restart
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
 
 end
