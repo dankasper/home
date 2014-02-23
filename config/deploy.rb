cb3db35b3c2fb5d3c8f4c1a0448a8b81d20cd611
Daniel Kasper <dankasper@gmail.com>
2014-02-23 01:27:59 -0800

    Syntax fixes

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,18 +4,18 @@ lock '3.1.0'
 set :application, 'danielkasper.net'
 set :repo_url,    'git@github.com:dankasper/home.git'
 set :deploy_to,   '/srv/home'
-set :default_env, path:   "#{deploy_to}/sbin:#{deploy_to}/go/bin",
+set :default_env, path:   "#{deploy_to}/sbin:#{deploy_to}/go/bin:$PATH",
                   goroot: "#{deploy_to}/go"
 
 namespace :deploy do
 
-  before :stop_nginx, :update_config do
+  task :update_config do
     on roles(:app) do
-      cp "#{current_path}/config/nginx.conf", "#{deploy_to}/conf"
+      execute :cp, "#{current_path}/config/nginx.conf", "#{deploy_to}/conf"
     end
   end
 
-  before :restart, :stop_nginx do
+  task :stop_nginx do
     on roles(:app) do
       execute <<-COMMAND
         cd #{deploy_to}
@@ -25,9 +25,10 @@ namespace :deploy do
       COMMAND
     end
   end
+  before :stop_nginx, :update_config
 
   desc 'Restart application'
-  task :restart do
+  task :restart => :stop_nginx do
     on roles(:app) do
       within fetch(:deploy_to) do
         execute 'nginx'
