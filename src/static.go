b05392228412c4075be341369acc80751117a0c7
Daniel Kasper <dankasper@gmail.com>
2014-03-25 09:30:34 -0700

    Added FCGI server with hello world page

---
+++ src/static.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+    "net"
+    "net/http"
+    "net/http/fcgi"
+)
+
+// replace with a real http.Handler
+type FastCGIServer struct{}
+
+func (s FastCGIServer) ServeHTTP(resp http.responseWriter, req *http.Request) {
+    resp.Write([]byte("<h1>Hello World</h1>"))
+}
+
+func main() {
+    listener, _ := net.Listen("tcp", ":9000")
+    srv := new(FastCGIServer)
+    fcgi.Serve(listener, srv)
+}
