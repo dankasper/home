68d010359d1777760d88eb4c076079c7f1de0056
Daniel Kasper <dankasper@gmail.com>
2014-05-17 18:57:40 -0700

    Use explicit hash for dependencies configuration value

--- config/dependencies.rb
+++ config/dependencies.rb
@@ -1,9 +1,10 @@
-set :dependencies, 'nginx' => {
-	             :repo_url => 'git@github.com/nginx/nginx.git',
-		     :branch   => 'nginx-1.6',
-		     :script   => "#{current_path}/install/nginx.sh"
-                   },
-                   'go' => {
-                     :source_dir => "#{current_path}/install"
-                     :script     => "#{current_path}/install/go.sh"
-		   }
+set :dependencies, { 'nginx' => {
+                       :repo_url => 'git@github.com/nginx/nginx.git',
+	               :branch   => 'nginx-1.6',
+		       :script   => "#{current_path}/install/nginx.sh"
+                     },
+                     'go' => {
+                       :source_dir => "#{current_path}/install"
+                       :script     => "#{current_path}/install/go.sh"
+                     }
+                   }
